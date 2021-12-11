44492b1b77c57e1876817c8e141c86069bd5f844
ksilawan <74664668+ksilawan@users.noreply.github.com>
2021-12-12 00:26:55 +0800

    screen done

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("hi")
-print("awit lods")
+import sys
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((601, 999.5))
+pygame.display.set_caption("Pusod ni lanz")
+bgcolor = (200, 200, 200)
+screen.fill(bgcolor)
+continuePlay = True
+
+
+while continuePlay:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            continuePlay = False
+
+pygame.display.update()
+
+pygame.quit()
+sys.exit()
+
