4247ce8fad03fe0b429950cf8f9df41401a9336f
ksilawan <74664668+ksilawan@users.noreply.github.com>
2021-12-12 00:39:05 +0800

    screen done

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ import sys
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((601, 999.5))
+screen = pygame.display.set_mode((601, 780))
 pygame.display.set_caption("Pusod ni lanz")
 bgcolor = (200, 200, 200)
 screen.fill(bgcolor)
 continuePlay = True
 
+platform = pygame.image.load("images/plat2.png")
+platPosX = 370
+platPosY = 480
+
+def plat_position(x, y):
+    screen.blit(platform, (x, y))
 
 while continuePlay:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             continuePlay = False
 
-pygame.display.update()
+
+    plat_position(platPosX, platPosY)
+    pygame.display.update()
 
 pygame.quit()
 sys.exit()
